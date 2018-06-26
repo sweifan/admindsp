a06d9640d7650392f45030aaf224f00e738905d2
tea-cafe <tea_cafe@163.com>
2018-06-26 19:11:56 +0800

    ok

---
+++ ui/views/upload_form.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title>Upload Form</title>
+</head>
+<body>
+
+ 
+<form action="/api/Tools/upload" enctype="multipart/form-data" method="post" accept-charset="utf-8">
+
+<input type="file" name="userfile" size="20" />
+
+<br /><br />
+
+<input type="submit" value="upload" />
+
+</form>
+
+</body>
+</html>
